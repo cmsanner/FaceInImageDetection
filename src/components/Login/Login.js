d682b12c3922f416a73bf02a7a15fdca7723be2c
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-22 11:13:37 -0500

    add onChange events for form fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -33,12 +33,18 @@ class Login extends React.Component {
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email"  id="email" />
+                                <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="email" name="email"  id="email" 
+                                    onChange={this.onEmailChange}
+                                />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="password" name="password"  id="password" 
+                                    onChange={this.onPasswordChange}
+                                />
                             </div>
                         
                         </fieldset>
