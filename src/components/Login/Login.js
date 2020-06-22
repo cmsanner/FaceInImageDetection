a5eca921203b2e6a6ff1a77e62079cb16b644828
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-22 11:07:35 -0500

    turn login into a class - need state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,60 @@
 import React from 'react';
 
 // make onClick a function that calls onRouteChange, otherwise this becomes an endless loop!
-const Login = ({onRouteChange}) => {
-    return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="emailAddress"  id="emailAddress" />
+//make this a class because we need to have state, change onRouteChange -> this.props.onRouteChange or use destructuring as the first line
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            logInEmail: '',
+            logInPassword: ''
+        }
+    }
+
+    onEmailChange = (event) => {
+        this.setState({logInEmail: event.target.value})
+    }
+    onPasswordChange = (event) => {
+        this.setState({logInPassword: event.target.value})
+    }
+
+    onSubmitLogIn = () => {
+        console.log(this.state)
+        // () => onRouteChange('home')
+        this.props.onRouteChange('home');
+    }
+
+    render(){
+        const {onRouteChange} = this.props;
+        return (
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 black-80">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email"  id="email" />
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                            </div>
+                        
+                        </fieldset>
+                        <div className="">
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                type="submit" value="Sign in" onClick={this.onSubmitLogIn}/>
                         </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                        <div className="lh-copy mt3">
+                        <p className="f6 link dim black db pointer" onClick={() => onRouteChange('register')}>Register</p>
+                    
                         </div>
-                       
-                    </fieldset>
-                    <div className="">
-                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" value="Sign in" onClick={() => onRouteChange('home')}/>
                     </div>
-                    <div className="lh-copy mt3">
-                    <p className="f6 link dim black db pointer" onClick={() => onRouteChange('register')}>Register</p>
-                   
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
+                </main>
+            </article>
+        )
+    }
  
 }
 
